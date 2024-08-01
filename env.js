c67f7f612e3cd6f3ed4a6cddc86c2f25d60617b2
Mishika-15 <87035950+Mishika-15@users.noreply.github.com>
2024-08-01 16:44:15 +0530

    add medicare files

---
+++ env.js
@@ -0,0 +1,15 @@
+export const dialogflowConfig = {
+  type: 'service_account',
+  project_id: 'medicare-v1-chpu',
+  private_key_id: 'c24925abf544ece475c09e69c0c08590ec990020',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC66CsWmkivsa/a\nwX7Sv8yV1ewi3QMl1T9xZ9sZgW/0TdYpUfcAoTIrO79V+GNjwGISp9HfbCLVTeoe\nigSMPVA3i6DDdDPHkL0kCx/GoxspthBVJWT63cXxgTcAeaIJwS/5P9ihkkd3RWXO\nfqmlZeb6hdOdGgtp7P+nBQZEMcq1Z1y22D6oy48y05i4Y1OBu7dlVNLgeYA0pUnL\nkMkRCGU/yK9iw7yG41ShuV0EnqGa/Ij8hT0LynBWtG5uGvwXv3n4KardaCuuC1ee\n/Rqy9MwAh+YsrADO7dm0xNGWqulhw25zCgQ6BxBtyTeQjFrdnwsqwERi3SYCI1kv\nHIEMBOJ9AgMBAAECggEAPnug8/Rt+jZHmLT5BdFSvoB+aYVDKSGqDOpwR42K9LwU\nrUmWbmJ1GBjoNQBNkapd3ui1c6B94JskekMflBZnO7p06wJs0PllEaiaXdhnG0Gf\nIifXIzqCkuLklUcUUXiA5i1e6VyygX/0sdyMMBvp5LRacvmi+S1epeI7Hw8aOhwZ\nhgLFH2EFMnI4b96eGJ1/X+Tb548gA/AHExJgO3PFOKk6UmozsAognAd6av6Fid6z\nC7Jj7MpgahGhvK2WGobRq6PgQqlvSJjuDp92RL9rSCg8dk2t4S9sAdB+zaGL4pkg\neSmaUdMTLuGaG4/LshWBUzbmQO1rKAfYkc2wI8jgbQKBgQD2rYxfxwjHA7MAQRrY\nmMyezgROU2pt08Vn9M4CWWnCarUqmml0IU6QuIKDCxpy+RwU2Pl9q1UNQ84cSBiL\nfRGzvD2iIkhIE2n/Ay5ejK1cxOUyzRLBcuqU43n7nG7zyJe4AdhwfYJ+gJLFdnyK\np97S/xJoZTHSUtsFgu4QthgvjwKBgQDB+F+dv+icO4UY1T03apeT6yBJJCFFE9w2\nikR/BaBteZdtatt3tI9k+JQgCtJzoze9RMuh2PKWDIfvJvDGcEygH/o+bXLB8KaG\nx7Nt2RH6B1onpDudqw5j8VxnBKQlUSJsKL9megEDDu/tv1HOM5w2zomtlNCKqumU\nj9HTmnGHMwKBgFnQusmsuH9t02EGU3YFvIsdy+eQ67FtZ/T/53pnf+Bv5eLLMhY8\nglSjObHo+wpdtWHJ1p1xIpif1Y3qmjU/ppBgu7gq71J+7xsQ13NJmV2ruyASFwAk\njVEEukdon+VXZhZzZZEfwcG5orcO+y2pDbKZeORV5BwGWwTYtUGUa531AoGAeZBg\n1Gu+q5N94cdam015e+HDFTPIihNdUu0q6nSrS5EFvzRWIVqO99f2dXJy3LgxKbRg\n7qkZt8JYuyArjCI7U6tW2SMc5cEdjKDBqhl987YB4ZBDMJhQ54APyhyVhzGnIiQT\neHUEB1eoNNdPvKyJvauJi7kVTaK5yzqrZTOI7RMCgYBA2yNvKHweWTuK6rgGSplQ\nXDEhegzvragBSUg9UTPfFAIh+M5g2cDUgBlYr+8BfEhtk9LV+gqngdBFaI4+nlQT\ntj7x957mLu3KBiQd2x8SQuq7kPxuZsliFRoz0o8ST4MCcCbK2K47BXURcDFKMScN\nsW8YTjPk8U8K8QYHiDIuEQ==\n-----END PRIVATE KEY-----\n',
+  client_email:
+    'sparsh-17bit1129-abes-ac-in@medicare-v1-chpu.iam.gserviceaccount.com',
+  client_id: '108218747408674716452',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/sparsh-17bit1129-abes-ac-in%40medicare-v1-chpu.iam.gserviceaccount.com',
+};
